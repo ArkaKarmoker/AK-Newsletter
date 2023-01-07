034b242802d76b623e3e9e435663de699d10bd28
Arka Karmoker <91338507+ArkaKarmoker@users.noreply.github.com>
2023-01-08 01:16:07 +0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,76 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+const request = require("request");
+const https = require("https");
+
+const app = express();
+
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
+
+app.get("/", function(req, res) {
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", function(req, res) {
+  const firstName = req.body.fName;
+  const lastName = req.body.lName;
+  const email = req.body.email;
+
+  const data = {
+    members: [{
+      email_address: email,
+      status: "subscribed",
+      merge_fields: {
+        FNAME: firstName,
+        LNAME: lastName
+
+      }
+    }]
+  };
+
+  const jsonData = JSON.stringify(data);
+
+  // const url = "https://<dc>.api.mailchimp.com/3.0/fa4510e5bf";
+  const url = "https://us14.api.mailchimp.com/3.0/lists/fa4510e5bf";
+
+  const options = {
+    method: "POST",
+    auth: "ArkaKarmoker:e61e7295fb10fd93ea912ed029144e3b-us14"
+  }
+
+  const request = https.request(url, options, function(response) {
+
+    if (response.statusCode === 200) {
+      res.sendFile(__dirname + "/success.html");
+    } else {
+      res.sendFile(__dirname + "/failure.html");
+    }
+
+    response.on("data", function(data) {
+      console.log(JSON.parse(data));
+    });
+  });
+
+  request.write(jsonData);
+  request.end();
+
+});
+
+app.post("/failure", function(req, res) {
+  res.redirect("/");
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  console.log("Server is running on port 3000");
+});
+
+// API Key
+// e61e7295fb10fd93ea912ed029144e3b-us14
+
+// List ID
+// fa4510e5bf
